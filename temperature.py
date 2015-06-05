cf813eaa67e7820732f014cc17d4d75970fc0206
Eslam Ali <talvikahvi@gmail.com>
2015-06-05 19:15:41 +0200

    Switched to using getopt for handling arguments.

--- temperature.py
+++ temperature.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
+import getopt
+import json
+import time
 import sys
 import urllib.request
-import json
 
 def getWeatherData(city, units="standard"):
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units
     weather = urllib.request.urlopen(url).read()
-    weatherString = weather.decode("utf-8")
-    weatherJson = json.loads(weatherString)
-    return weatherJson
+    weather = weather.decode("utf-8")
+    return json.loads(weather)
 
 def printTemp(data, units):
     if data['cod'] == "404":
@@ -32,19 +33,41 @@ def printTemp(data, units):
 def help():
     print("First mandatory argument: City name, e.g. Cairo,EG or Helsinki")
     print("Second optional argument: [s]tandard, [m]etric, [i]mperial")
-    sys.exit(-1)
 
-if len(sys.argv) < 2 or len(sys.argv) > 3:
-    help()
-else:
-    city = str(sys.argv[1])
-    
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hc:smid", ["city="])
+    except getopt.GetoptError as err:
+        help()
+        sys.exit(2)
     units = "metric"
-    if len(sys.argv) == 3:
-        if sys.argv[2].startswith("s"):
+    daemon = False
+    city = False
+    for o, a in opts:
+        if o == "-s":
             units = "standard";
-        elif sys.argv[2].startswith("i"):
-            units = "imperial"
-        
+        elif o == "-m":
+            units = "metric";
+        elif o == "-i":
+            units = "imperial";
+        elif o == "-d":
+            daemon = True;
+        elif o == "-h":
+            help()
+            sys.exit(0)
+        elif o == "-c" or o == "--city":
+            city = a;
+
+    if not city:
+        help()
+        sys.exit(2)
+
     weather = getWeatherData(city, units)
     printTemp(weather, units)
+    return
+
+def daemonize():
+    return
+
+if __name__ == "__main__":
+    main()
