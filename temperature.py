a6689dff810c4bd53d64eb42190946a564f86a92
Eslam Ali <talvikahvi@gmail.com>
2015-06-22 21:37:22 +0200

    * Added the openweathermap.org API key. * Fixed the argument processing. * Fixed the file handing. * Fixed the daemonization behavior.

--- temperature.py
+++ temperature.py
@@ -6,11 +6,17 @@ import sys
 import urllib.request
 
 def help():
-    print("First mandatory argument: City name, e.g. Cairo,EG or Helsinki")
-    print("Second optional argument: [s]tandard, [m]etric, [i]mperial")
+    print("Name: Eslam's temperature script, using openweathermap.org")
+    print("Synopsis: temperature.py [ -m|-i|-s ] [ -c CITY ] [ -d -o ]")
+    print("Options:")
+    print("\t-m: Metric units, -i: Imperial units, -s: Kelvin")
+    print("\t-c: City name, e.g. Cairo, Helsinki")
+    print("\t-d: Daemonize, see -f")
+    print("\t-f: Filename to output the weather data to, see -d")
 
 def getWeatherData(city, units="standard"):
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units
+    url += "&APPID=019c6096c37a2e9a4561ac67304a2846"
     weather = urllib.request.urlopen(url).read()
     weather = weather.decode("utf-8")
     return json.loads(weather)
@@ -28,16 +34,21 @@ def weatherString(data, units):
 
     temp = data['main']['temp']
     humidity = data['main']['humidity']
+    city = data['name']
 
-    weather = "Temperature: " + str(int(temp)) + unitString + "\n"
-    weather += "Humidity: " + str(humidity) + "%"
+    weather = city + "'s temperature: " + str(int(temp)) + unitString
+    weather += "| Humidity: " + str(humidity) + "%"
     return weather
 
-def daemonize(city, units):
+def daemonize(city, units, forecast_file):
     while True:
         weather = getWeatherData(city, units)
-        f = open('forecast', 'w')
         forecast = weatherString(weather, units)
+        try:
+            f = open(forecast_file, 'w')
+        except IOError:
+            print("Unable to open the file: " + forecast_file)
+            sys.exit(2)
         if forecast:
             f.write(forecast)
             f.close()
@@ -46,13 +57,14 @@ def daemonize(city, units):
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hc:smid", ["city="])
+        opts, args = getopt.getopt(sys.argv[1:], "hc:smido:", ["city=", "out="])
     except getopt.GetoptError as err:
         help()
         sys.exit(2)
     units = "metric"
     daemon = False
     city = False
+    forecast_file = "/tmp/forecast_" + str(os.getpid())
     for o, a in opts:
         if o == "-s":
             units = "standard";
@@ -67,6 +79,8 @@ def main():
             sys.exit(0)
         elif o == "-c" or o == "--city":
             city = a;
+        elif o == "-o" or o == "--output":
+            forecast_file = a;
         else:
             assert False, "unhandled option"
 
@@ -75,7 +89,11 @@ def main():
         sys.exit(2)
 
     if daemon:
-        daemonize(city, units);
+        if not forecast_file:
+            print("No output file specified")
+            sys.exit(2)
+        else:
+            daemonize(city, units, forecast_file);
     else:
         weather = getWeatherData(city, units)
         weather = weatherString(weather, units)
